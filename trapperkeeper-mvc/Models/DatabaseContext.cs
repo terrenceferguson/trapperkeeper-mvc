bbe19e319a6051ca269d0d239b58295140974dbb
Terrence Ferguson <me@terrenceferguson.net>
2019-05-14 21:33:21 -0500

    Added full code first functionality to project. Switched MySql package to Pomelo(sp?), so it would handle the EF Migrations table for me. Renamed the context to DatabaseContext for simplicity. Swithced to VSCode to have more functionality for the front end, as well as it being considerably more lightweight than VS for Mac.

---
+++ trapperkeeper-mvc/Models/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using trapperkeeper_mvc.Models.Finances;
+using Microsoft.EntityFrameworkCore;
+
+namespace trapperkeeper_mvc.Models
+{
+    public class TrapperKeeperContext : DbContext
+    {
+        public TrapperKeeperContext() => Database.EnsureCreated();
+
+        public TrapperKeeperContext(DbContextOptions options) : base(options) => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySql("server=localhost;database=finances;user=trapperkeeper;password=Hudson666!");
+
+
+        public DbSet<TransactionLedger> TransactionLedger { get; set; }
+        public DbSet<TransactionEntry> TransactionEntry { get; set; }
+    }
+}
